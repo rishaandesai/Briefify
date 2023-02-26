685391b2e7a3fcfee9d9a0d65a60efd5b0fa0cb4
Rishaan Desai <77017806+Blackhole1123@users.noreply.github.com>
2023-02-25 17:23:48 -0800

    fix

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
 from transformers import PegasusTokenizer, pipeline
 from summarizer import read_article
 import json
