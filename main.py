3e55c35c7edd23bf25706150663ba9f1cddc069a
Rishaan Desai <77017806+Blackhole1123@users.noreply.github.com>
2023-02-26 11:58:13 -0800

    fix

--- main.py
+++ main.py
@@ -93,6 +93,3 @@ def summarizetext():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080, debug=True)
